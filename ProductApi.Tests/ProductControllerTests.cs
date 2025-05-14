7ecc2d447dd53657f64e860240703cd051ad608b
rohannegi1104 <rohansingh6814@gmail.com>
2025-05-14 17:46:04 +0530

    Update ProductControllerTests.cs

--- ProductApi.Tests/ProductControllerTests.cs
+++ ProductApi.Tests/ProductControllerTests.cs
@@ -85,7 +85,7 @@ namespace ProductApi.Tests
 
         [Fact]
         public void GetAll_AfterAdding_ReturnsOne()
-        {
+        
             _controller.Add(new Product { Name = "Prod1", Price = 5 });
             var result = _controller.GetAll() as OkObjectResult;
             var products = result?.Value as IEnumerable<Product>;
@@ -252,25 +252,25 @@ namespace ProductApi.Tests
         }
  
         // Add 10 more tests
-        [Fact] 
-        public void Add_Product_SetsIdCorrectly() { var p = new Product { Name = "A", Price = 1 }; _controller.Add(p); Assert.Equal(1, p.Id); }
-        [Fact] 
-        public void Add_ThenDelete_Product_NotFoundOnGet() { var p = new Product { Name = "B", Price = 1 }; _controller.Add(p); _controller.Delete(p.Id); var res = _controller.Get(p.Id); Assert.IsType<NotFoundResult>(res); }
-        [Fact] 
-        public void Update_Product_ChangesName() { var p = new Product { Name = "X", Price = 5 }; _controller.Add(p); p.Name = "Y"; _controller.Update(p.Id, p); var res = (_controller.Get(p.Id) as OkObjectResult)?.Value as Product; Assert.Equal("Y", res?.Name); }
-        [Fact] 
-        public void Add_NullName_StillAdds() { var p = new Product { Name = null, Price = 5 }; _controller.Add(p); var res = _controller.Get(p.Id) as OkObjectResult; Assert.NotNull(res); }
-        [Fact] 
-        public void Add_Product_WithNegativePrice() { var p = new Product { Name = "N", Price = -1 }; _controller.Add(p); var res = _controller.Get(p.Id) as OkObjectResult; Assert.Equal(-1, ((Product)res?.Value!).Price); }
-        [Fact]
-        public void Delete_EmptyList_NoCrash() { var result = _controller.Delete(1); Assert.IsType<NoContentResult>(result); }
-        [Fact]
-        public void GetAll_MultipleProducts_ReturnsCorrectCount() { _controller.Add(new Product { Name = "1", Price = 1 }); _controller.Add(new Product { Name = "2", Price = 2 }); var result = _controller.GetAll() as OkObjectResult; Assert.Equal(2, ((IEnumerable<Product>)result!.Value!).Count()); }
-        [Fact]
-        public void Update_NonExistent_DoesNothing() { var p = new Product { Id = 99, Name = "Ghost", Price = 0 }; var result = _controller.Update(99, p); Assert.IsType<NoContentResult>(result); }
-        [Fact]
-        public void Add_Product_CheckPrice() { var p = new Product { Name = "P", Price = 999.99M }; _controller.Add(p); var res = (_controller.Get(p.Id) as OkObjectResult)?.Value as Product; Assert.Equal(999.99M, res?.Price); }
-        [Fact]
-        public void Add_DuplicateNames_Allowed() { _controller.Add(new Product { Name = "Same", Price = 1 }); _controller.Add(new Product { Name = "Same", Price = 2 }); var result = _controller.GetAll() as OkObjectResult; Assert.Equal(2, ((IEnumerable<Product>)result!.Value!).Count()); }
+        // [Fact] 
+        // public void Add_Product_SetsIdCorrectly() { var p = new Product { Name = "A", Price = 1 }; _controller.Add(p); Assert.Equal(1, p.Id); }
+        // [Fact] 
+        // public void Add_ThenDelete_Product_NotFoundOnGet() { var p = new Product { Name = "B", Price = 1 }; _controller.Add(p); _controller.Delete(p.Id); var res = _controller.Get(p.Id); Assert.IsType<NotFoundResult>(res); }
+        // [Fact] 
+        // public void Update_Product_ChangesName() { var p = new Product { Name = "X", Price = 5 }; _controller.Add(p); p.Name = "Y"; _controller.Update(p.Id, p); var res = (_controller.Get(p.Id) as OkObjectResult)?.Value as Product; Assert.Equal("Y", res?.Name); }
+        // [Fact] 
+        // public void Add_NullName_StillAdds() { var p = new Product { Name = null, Price = 5 }; _controller.Add(p); var res = _controller.Get(p.Id) as OkObjectResult; Assert.NotNull(res); }
+        // [Fact] 
+        // public void Add_Product_WithNegativePrice() { var p = new Product { Name = "N", Price = -1 }; _controller.Add(p); var res = _controller.Get(p.Id) as OkObjectResult; Assert.Equal(-1, ((Product)res?.Value!).Price); }
+        // [Fact]
+        // public void Delete_EmptyList_NoCrash() { var result = _controller.Delete(1); Assert.IsType<NoContentResult>(result); }
+        // [Fact]
+        // public void GetAll_MultipleProducts_ReturnsCorrectCount() { _controller.Add(new Product { Name = "1", Price = 1 }); _controller.Add(new Product { Name = "2", Price = 2 }); var result = _controller.GetAll() as OkObjectResult; Assert.Equal(2, ((IEnumerable<Product>)result!.Value!).Count()); }
+        // [Fact]
+        // public void Update_NonExistent_DoesNothing() { var p = new Product { Id = 99, Name = "Ghost", Price = 0 }; var result = _controller.Update(99, p); Assert.IsType<NoContentResult>(result); }
+        // [Fact]
+        // public void Add_Product_CheckPrice() { var p = new Product { Name = "P", Price = 999.99M }; _controller.Add(p); var res = (_controller.Get(p.Id) as OkObjectResult)?.Value as Product; Assert.Equal(999.99M, res?.Price); }
+        // [Fact]
+        // public void Add_DuplicateNames_Allowed() { _controller.Add(new Product { Name = "Same", Price = 1 }); _controller.Add(new Product { Name = "Same", Price = 2 }); var result = _controller.GetAll() as OkObjectResult; Assert.Equal(2, ((IEnumerable<Product>)result!.Value!).Count()); }
     }
 }
